674f823839daf516bce3b298941b466573b1a5ed
Brian Rosengarten <barosengarten@gmail.com>
2018-12-27 18:36:31 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,17 +6,7 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-#    10.times {super}
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
+    10.times {super}
   end
 
 end
